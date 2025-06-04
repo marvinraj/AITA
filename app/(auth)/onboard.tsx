2a2e7f74f8fcbb3000a59586f098dd2f01309edd
Marvin <marvinraj77@gmail.com>
2025-06-04 11:36:51 +0800

    altered font size in onboard

--- app/(auth)/onboard.tsx
+++ app/(auth)/onboard.tsx
@@ -6,7 +6,7 @@ const Onboarding = () => {
   return (
     <View className="flex-1 bg-primaryBG px-6">
       <View className="items-center mt-6 mb-8">
-        <Text className="text-6xl text-primaryFont text-center font-BellezaRegular">AITA</Text>
+        <Text className="text-5xl text-primaryFont text-center font-BellezaRegular">AITA</Text>
       </View>
       <View className="flex-1 items-center justify-center">
         <Image
